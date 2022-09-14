eacef563ee38aaad51a7fcdd8e82bac856fe2333
InAe-Lee <106795795+inae0615@users.noreply.github.com>
2022-09-14 13:37:16 +0900

    Update thermal_db.py

--- thermal_db.py
+++ thermal_db.py
@@ -8,19 +8,15 @@ sensor = Adafruit_AMG88xx(busnum=1)
 
 #db 연동/연결하기 
 conn = pymysql.connect(host="192.168.1.164",user="raspi_inae",passwd="12341234",db="sensor")
-
 #host : ip주소
 #user : user_name 
 #passwd : password 
 #db : database_name 
 
 try :
-
      #db 접속 
      with conn.cursor() as cur:
-
-        while True :
-    
+        while True :    
             temperature = sensor.readThermistor()
             #temperature : amg8833에서 불러온 온도값 
           
@@ -30,15 +26,12 @@ try :
                 
                 #db 삽입
                 cur.execute("insert into thermal (date, temperature) values(default, {0:0.2f})".format(temperature))
-                conn.commit()
-                
+                conn.commit()                
                 #thermal 테이블에 date(날짜), tenmperature(float형식)값 추가하기 
-               
-               
+                              
             else:
                 print("Failed to get reading.")
-            time.sleep(3)
-            #3초 간격으로 반복 
+                time.sleep(10) #10초 간격으로 반복 
                
 except KeyboardInterrupt :
        exit()
